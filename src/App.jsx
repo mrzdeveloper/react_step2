c68c6618224245d455c9334cc3665193c785dabe
mrzdeveloper <zendegani.dev@gmail.com>
2022-12-08 14:54:17 +0330

    step3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react';
 import Fproduct from './Products';
 const App = () => {
+
+    const [products, setproducts] = useState([
+        { id: 1, productname: 'pc', count: 8 },
+        { id: 2, productname: 'mm', count: 99 },
+        { id: 3, productname: 'pmc', count: 9 },
+        { id: 4, productname: 'kk', count: 8 },
+        { id: 5, productname: 'llll', count: 77 },
+        { id: 6, productname: 'plyuc', count: 3 },
+        { id: 7, productname: '234', count: 6 }
+    ])
+
     return (
         <>
-            <Fproduct />
-            <Fproduct />
-            <Fproduct />
-            <Fproduct />
+            {products.map((p, index) => (
+                <Fproduct key={index} id={p.id} productname={p.productname} count={p.count} >
+                    <p>hi product</p>
+                </Fproduct>
+
+            ))}
         </>
     );
+
+
+
+
 }
 
 export default App;
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react';
+import './style.css'
 
-const Fproduct = () => {
+const Fproduct = (props) => {
 
-    const [count, setcount] = useState()
+    const [count, setcount] = useState(props.count)
     return (
         <>
             <div>
-
-                <span className='m-2 '>ProductName</span>
-                <span className='m-2 bg-primary badge'>{format()}</span>
-                <button onClick={inc} className='m-2 btn btn-sm btn-primary'>+</button>
-                <button onClick={dec} className='m-2 btn btn-sm btn-warning'>-</button>
-                <button onClick={delet} className='m-2 btn btn-sm btn-danger'>Delete</button>
+                <span className='m-2 '>{props.id}</span>
+                <div className='d-inline'>{props.productname}</div>
+                <div className='xa'>
+                    <div className='xa bg-primary badge'>{format()}</div>
+                    <button onClick={inc} className='m-2 btn btn-sm btn-primary'>+</button>
+                    <button onClick={dec} className='m-2 btn btn-sm btn-warning'>-</button>
+                    <button onClick={delet} className='m-2 btn btn-sm btn-danger'>Delete</button>
+                </div>
             </div>
         </>
 
