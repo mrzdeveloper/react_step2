bff82a69e20ade7640a8bf84ae9891997cb74934
mrzdeveloper <zendegani.dev@gmail.com>
2022-12-08 10:59:19 +0330

    step2

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,34 +1,40 @@
-import { Component } from 'react';
 import { useState } from 'react';
 
 const Fproduct = () => {
-    const count = 0;
+
+    const [count, setcount] = useState()
     return (
         <>
             <div>
 
                 <span className='m-2 '>ProductName</span>
-                <span className='m-2 bg-primary badge '>{format()}</span>
-                <button onClick={inc()} className='m-2 btn btn-sm btn-primary'>+</button>
-                <button onClick={dec()} className='m-2 btn btn-sm btn-warning'>-</button>
-                <button onClick={delet()} className='m-2 btn btn-sm btn-danger'>Delete</button>
+                <span className='m-2 bg-primary badge'>{format()}</span>
+                <button onClick={inc} className='m-2 btn btn-sm btn-primary'>+</button>
+                <button onClick={dec} className='m-2 btn btn-sm btn-warning'>-</button>
+                <button onClick={delet} className='m-2 btn btn-sm btn-danger'>Delete</button>
             </div>
         </>
 
     );
 
     function inc() {
-        count += 1;
+        setcount(count + 1)
+
     }
 
     function dec() {
-        count -= 1;
+        if (count <= 0) {
+            setcount(0)
+        } else
+            setcount(count - 1)
+
     }
     function delet() {
+        setcount(0)
 
     }
     function format() {
-        if (count === 0) {
+        if (count <= 0) {
             return 'zero'
         } else {
             return count
